86665b0daf19da8c25a04d3b75c73ae6182ebfe2
LoganLS <logan_56@hotmail.fr>
2022-12-13 12:27:20 +0100

    feat: format date

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 import HeaderNav from '@/components/HeaderNav.vue'
 import { getRdvs, RDV } from '@/services/rdv'
-import { getStorage, ref as refStorage, getDownloadURL } from '@firebase/storage';
 import { onMounted, ref } from 'vue';
+import * as dayjs from 'dayjs'
 
 const rdvs = ref<Array<RDV>>([])
 onMounted(async () => {
     rdvs.value = await getRdvs()
 })
 
-// async function getStorageUrl(fileName: string) {
-//     const storage = getStorage()
-//     const storageRef = refStorage(storage, fileName)
-//     return await getDownloadURL(storageRef)
-// }
+function getDay(date: string): string {
+    return dayjs(date).format('DD/MM/YYYY à HH:mm')
+}
 
 </script>
 
@@ -21,10 +19,10 @@ onMounted(async () => {
     <section>
         <HeaderNav />
         <section>
-            <h1>Liste RDV</h1>
+            <h1>Liste de mes RDVs</h1>
             <ul>
                 <li v-for="rdv in rdvs">
-                    <span>{{ rdv.date }}</span>
+                    <span>{{ getDay(rdv.date) }}</span>
                     <span v-if="rdv.link" class="ml-5">
                         <a :href="rdv.link" target="_blank" :download="rdv.link">Télécharger le rdv dans son calendrier</a>
                     </span>
