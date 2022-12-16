e3534a9c86ad2496112c343bbe6e77a24e6ab3ad
LoganLS <logan_56@hotmail.fr>
2022-12-16 09:37:08 +0100

    feat: spash screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import LoadingScreen from '@/components/LoadingScreen.vue'
 import { RouterView } from 'vue-router'
+
+const isLoading = ref<Boolean>(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    console.log('coucou')
+    isLoading.value = false
+  }, 1500)
+})
 </script>
 
 <template>
   <VApp class="bg-background">
-    <RouterView />
+    <LoadingScreen v-if="isLoading" />
+    <RouterView v-else />
   </VApp>
 </template>
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,9 @@
+<template>
+    <VRow align="center">
+        <VCol align="center">
+            <VIcon>
+                mdi-cellphone-marker
+            </VIcon>
+        </VCol>
+    </VRow>
+</template>
